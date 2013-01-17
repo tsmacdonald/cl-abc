b3c44dcfe875f1cb49b19f72747590b8638336ea
Tim Macdonald <tsmacdonald@gmail.com>
2013-01-17 17:28:11 -0600

    Finish basic object model; hook up to parse-file
    Lots of dummy methods though

--- cl-abc.lisp
+++ cl-abc.lisp
@@ -29,25 +29,54 @@
   (when (cl-ppcre:scan "[ABCDFGHIKLMmNOPQRrSsTUVWwXZ]:.*" line)
       line))
 
+(defun make-tune ()
+  (make-instance 'tune))
+
+(defgeneric add-metainformation (tune line)
+  (:documentation "Parses the given line and adds it to the tune"))
+
+(defmethod add-metainformation ((tune tune) (line string))
+  ;;;hahahahahahhaa
+  nil)
+
+(defgeneric add-melody-notes (tune note-seq)
+  (:documentation "Adds the given note sequence to the end of the tune."))
+
+(defmethod add-melody-notes ((tune tune) note-seq)
+  (setf (tune-melody tune) (append (tune-melody tune) note-seq)))
+
+(defun parse-body (line) ;; FIXME
+  (list (make-instance 'note :value 'C :octave 4) (make-instance 'note :value 'D :octave 4)))
+
 (defun parse-file (filename)
   "Main entry point to parse a given file. Returns a TUNE item."
-  (let ((metainformation (make-metainformation))
-	(body (make-body)))
+  (let ((tune (make-tune)))
     (with-open-file (file filename
 			  :direction :input)
       (loop for line = (clean-line (read-line file nil))
 	 while line do
 	   (format t "Parsing {~a}~&" line)
 	   (if (headerp line)
-	       (add-metainformation metainformation line)
-	       (add-notes body (parse-body line)))))
-    (make-tune metainformation body)))
+	       (add-metainformation tune line)
+	       (add-melody-notes tune (parse-body line)))))
+    tune))
+	   
 
+(defparameter *metainformation-slot-forms* 
+  (loop for property in *metainformation-fields*
+     collecting (list property
+		      :initarg (intern (symbol-name property) :keyword)
+		      :accessor (values (intern (string-upcase (concatenate 'string "tune-"
+									    (symbol-name property))))))))
 (eval
- `(defclass metainformation ()
-	 ,(loop for property in *metainformation-fields*
-		collecting (list property
-				 :initarg (intern (symbol-name property) :keyword)
-				 :accessor (values (intern (string-upcase (concatenate 'string "tune-"
-								(symbol-name property)))))))
-	 (:documentation "Describes the metainformation (title, key signature, etc.) of a tune")))
+ `(defclass tune ()
+    (,@*metainformation-slot-forms*
+     (melody :initarg :melody :initform '() :accessor tune-melody))
+    (:documentation "Fooooooo")))
+
+(defclass note ()
+  ((value :initarg :value :reader note-value)
+   (octave :initarg :octave :reader note-octave)
+   (accidental :initarg :accidental :initform 'natural :reader note-accidental)
+   (duration :initarg :duration :initform 1/8 :reader note-duration))
+  (:documentation "Represents a note."))
--- config.lisp
+++ config.lisp
@@ -1,1 +1,1 @@
-(:filenames ("test-files/john_turners_maggot.abc"))
+(:filenames ("~/src/cl-abc/test-files/john_turners_maggot.abc"))
